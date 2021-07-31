a3efb9f072f33bc887f33a63f689ddc5403e02b4
Yihan Song <yihan.song@sap.com>
2021-07-31 17:31:58 +0800

    Use output file instead of redirect to capture data

--- zap-playlist.sh
+++ zap-playlist.sh
@@ -3,7 +3,7 @@ TODAY=`date +%Y%m%d`
 
 curl -c cookie.txt "${NETEASE_MUSIC_API}/login?email=${NETEASE_MUSIC_USERNAME}&password=${NETEASE_MUSIC_PASSWORD}" > /dev/null 2>&1
 
-curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" > $TODAY.json
+curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" -o $TODAY.json
 
 curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/tracks?op=del&pid=${NETEASE_MUSIC_PLAYLIST_ID}&tracks=`jq  -r '[.playlist.trackIds[].id ] | join(",")' $TODAY.json`"
 
