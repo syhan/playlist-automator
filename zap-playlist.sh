434d05dd3b7311bb94649dad1026b0b85f6166ae
Yihan Song <yihan.song@sap.com>
2021-08-03 12:00:58 +0800

    Fix the incorrect path and add comments

--- zap-playlist.sh
+++ zap-playlist.sh
@@ -1,26 +1,38 @@
 #!/bin/bash
-set -e
-
-mkdir playlists
-# fetch previous playlist
-git clone --single-branch --branch gh-pages https://github.com/syhan/playlist-automator.git tmp
-mv tmp/*.json playlists/
-
-rm -rf tmp/
+set -e # in case of any error, stop executing the subsequence command
 
 TODAY=`date +%Y%m%d`
 
+mkdir playlists
+
+# fetch previous playlist
+git clone --single-branch --branch gh-pages https://github.com/syhan/playlist-automator.git tmp
+mv tmp/*.json playlists/
+rm -rf tmp/
+
+cd playlists
+
+# login
 curl -c cookie.txt "${NETEASE_MUSIC_API}/login?email=${NETEASE_MUSIC_USERNAME}&password=${NETEASE_MUSIC_PASSWORD}" > /dev/null 2>&1
 
+# reflect login status
 curl -b cookie.tst "${NETEASE_MUSIC_API}/login/status"
 
 # get current playlist
-curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" -o playlists/$TODAY.json
+curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" -o $TODAY.json
 
-cat $TODAY.json
+# for troubleshooting, could eliminate sometime later
+cat playlists/$TODAY.json
 
-tracks=`jq  -r '[.playlist.trackIds[].id | tostring] | join(",")' $TODAY.json`
+# delete all tracks extracted from the playlist
+tracks=`jq -r '[.playlist.trackIds[].id | tostring] | join(",")' $TODAY.json`
+# [ ! -z "$DRY_RUN" ] && echo "skip delete tracks from playlist" || curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/tracks?op=del&pid=${NETEASE_MUSIC_PLAYLIST_ID}&tracks=$tracks"
+rm cookie.txt # then we don't need the cookie anymore, delete for safety purpose
 
-# curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/tracks?op=del&pid=${NETEASE_MUSIC_PLAYLIST_ID}&tracks=$tracks"
+# extract today playlist title and description
+jq -r '.playlist | (.name + "\n" + .description)' $TODAY.json > today.txt
 
-rm cookie.txt
+# tracks with name and artist 
+jq -r '.playlist.tracks[] | (.name + " " + ([.ar[].name] | join("/")))' $TODAY.json >> today.txt
+
+cd ..
