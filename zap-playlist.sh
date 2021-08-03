2a0301999c11cb13af4b604cdad34eba296755eb
Yihan Song <syhannnn@gmail.com>
2021-08-03 11:07:27 +0800

    Incremental update the daily playlist

--- zap-playlist.sh
+++ zap-playlist.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 set -e
+
+# fetch previous playlist
+git checkout gh-pages -- "*.json"
+mkdir playlists
+mv *.json playlists/
+
 TODAY=`date +%Y%m%d`
 
 curl -c cookie.txt "${NETEASE_MUSIC_API}/login?email=${NETEASE_MUSIC_USERNAME}&password=${NETEASE_MUSIC_PASSWORD}" > /dev/null 2>&1
 
 curl -b cookie.tst "${NETEASE_MUSIC_API}/login/status"
 
-curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" -o $TODAY.json
+# get current playlist
+curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/detail?id=${NETEASE_MUSIC_PLAYLIST_ID}" -o playlists/$TODAY.json
 
 cat $TODAY.json
 
-tracks=`cat $TODAY.json | jq  -r '[.playlist.trackIds[].id | tostring] | join(",")'`
+tracks=`jq  -r '[.playlist.trackIds[].id | tostring] | join(",")' $TODAY.json`
 
-curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/tracks?op=del&pid=${NETEASE_MUSIC_PLAYLIST_ID}&tracks=$tracks"
+# curl -b cookie.txt "${NETEASE_MUSIC_API}/playlist/tracks?op=del&pid=${NETEASE_MUSIC_PLAYLIST_ID}&tracks=$tracks"
 
 rm cookie.txt
